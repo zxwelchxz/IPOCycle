b2129efde1fadc016308c1362f7163df72a93969
XxTheWebHeadxX <xxtheamazingwebheadxx@gmail.com>
2014-04-09 18:08:35 +0000

    read input file created

--- IPO_Cycle.c
+++ IPO_Cycle.c
@@ -6,8 +6,30 @@
 # include <stdio.h>
 
 
-int main (void)
+int main (int argc, char *argv[])
 {
+	FILE *fp;
+	int i;
+	int k;
+	
+	for (i=0;i<argc;i++)
+	{
+		fp = fopen(argv[i], "r");
+		if (fp == NULL)
+		{
+			printf("Cannot open input file %s",argv[i]);
+			return 1;
+		}
+		
+		while ((k = getc(fp)) != EOF)
+		{
+			putchar(k);
+			printf(k);	
+		}
+		
+		fclose(fp);
+		
+	}
 	
 	return 0;
 }

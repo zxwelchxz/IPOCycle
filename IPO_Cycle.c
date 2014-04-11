f979430e860b462852fff07d64f67104aabd1345
Steven Welch <zxwelchxz@gmail.com>
2014-04-11 15:16:49 -0400

    Debugging for number of arguments

--- IPO_Cycle.c
+++ IPO_Cycle.c
@@ -9,15 +9,23 @@
 
 int main (int argc, char *argv[])
 {
+	//FILE *ERR = stdout;
 	FILE *fp;
 	int i;
 	char line[100];
 	
-	for (i = 1; i < argc; i++)
-	{
+	/* will not run with no arguments */
+	if (argc == "") {
+		printf("Usage: csc322a5 <infile> [<outfile>]");
+		return 0;
+	}
+	
+	//if (argc == 1) {
+		
+	
+	for (i = 1; i < argc; i++) {
 		fp = fopen(argv[i], "r");
-		if (fp == NULL)
-		{
+		if (fp == NULL) {
 			printf("Cannot open input file %s",argv[i]);
 			return 1;
 		}
@@ -25,7 +33,6 @@ int main (int argc, char *argv[])
 		while (fgets(line, sizeof(line), fp) != NULL)
 		{
 			fputs(line, stdout);
-			//printf("%s", line);
 		}
 		
 		fclose(fp);
